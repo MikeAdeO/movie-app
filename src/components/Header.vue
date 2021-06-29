6cb1da6bb91ceb21e895009212450e867145ef97
MikeAdeO <56716012+MikeAdeO@users.noreply.github.com>
2021-06-29 11:30:34 +0100

    Movie App

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+	<header>
+		<router-link to="/">
+			<h1><span>Michael</span>Movies</h1>
+		</router-link>
+	</header>
+</template>
+
+<script>
+	export default {
+		name: "header",
+	};
+</script>
+
+<style></style>
