54390db0f241f160b11b39d9b975e2f83dae8cff
MikeAdeO <56716012+MikeAdeO@users.noreply.github.com>
2021-07-03 14:20:27 +0100

    General Update
    updated the OMDb Api, work generally on the frontend

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,67 @@
 <template>
-	<div class="home">Home</div>
+	<div class="home">
+		<div class="featured-card">
+			<router-link to="/movie/tt0409591">
+				<img
+					src="https://m.media-amazon.com/images/M/MV5BMTE5NzIwMGUtYTE1MS00MDUxLTgyZjctOWVkZDAxM2M4ZWQ4XkEyXkFqcGdeQXVyNjc2NjA5MTU@._V1_SX300.jpg"
+					alt="Naruto Poster"
+					class="featured-image"
+				/>
+				<div class="detail">
+					<h3>Naruto</h3>
+					<p>
+						Donec rutrum congue leo eget malesuada. Donec rutrum congue leo eget
+						malesuada. Cras ultricies ligula sed magna dictum porta.
+					</p>
+				</div>
+			</router-link>
+		</div>
+		<form @submit.prevent="searchMovies()" class="search-box">
+			<input
+				type="text"
+				placeholder="What are you looking for?"
+				v-model="search"
+			/>
+			<input type="submit" value="Search" />
+		</form>
+		<div class="movies-list">
+			<div class="movie" v-for="movie in movies" :key="movie.imdbID">
+				{{ movie.Title }}
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+	import { ref } from "vue";
 	// @ is an alias to /src
 
 	export default {
 		name: "Home",
 		components: {},
+		setup() {
+			const search = ref("");
+			const movies = ref([]);
+			const searchMovies = () => {
+				if (search.value != "") {
+					// console.log(
+					// 	`http://www.omdbapi.com/?apikey=${process.env.VUE_APP_APIKEY}`
+					// );
+					fetch(
+						`http://www.omdbapi.com/?apikey=${process.env.VUE_APP_APIKEY}&s=${search.value}`
+					)
+						.then((response) => response.json())
+						.then((data) => {
+							movies.value = data.Search;
+							search.value = "";
+						});
+				}
+			};
+			return {
+				search,
+				movies,
+				searchMovies,
+			};
+		},
 	};
 </script>
---
+++ src/views/MovieDetail.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="movie-detail">Detail</div>
+</template>
+
+<script>
+	export default {
+		name: "Movie Detail",
+	};
+</script>
+
+<style></style>
